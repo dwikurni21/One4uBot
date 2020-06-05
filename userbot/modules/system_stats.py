8f39505444a8ea01c0ea5b67bc0b6ad743663a2d
dwikurni21 <66350943+dwikurni21@users.noreply.github.com>
2020-06-05 18:35:51 +0700

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -137,7 +137,7 @@ async def amireallyalive(alive):
 	                 "===================== \n"
                      f"`User : `{DEFAULTUSER} \n"
 		             "===================== \n"
-                     f"_Bolot__ \n")
+                     f"__DrianTAEK__ \n")
 
 
 @register(outgoing=True, pattern="^.aliveu")
