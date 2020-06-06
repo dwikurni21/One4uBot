3b622560f39cfc0d9916165cfb8a8e04ff834c03
dwikurni21 <66350943+dwikurni21@users.noreply.github.com>
2020-06-06 13:03:53 +0700

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -137,7 +137,7 @@ async def amireallyalive(alive):
 	                 "===================== \n"
                      f"`User : `{DEFAULTUSER} \n"
 		             "===================== \n"
-                     f"__Running in the dark__ \n") 
+                     f"__Running slow__ \n") 
 
 
 @register(outgoing=True, pattern="^.aliveu")
